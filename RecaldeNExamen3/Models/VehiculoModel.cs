6efb6ea9ae789c453d64ba2f51cf3c1103a708be
Nicolasre18 <nicolasre18@gmail.com>
2025-07-07 07:20:42 -0500

    Se creo el modelo para el ejercicio numero 9

---
+++ RecaldeNExamen3/Models/VehiculoModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace RecaldeNExamen3.Models
+{
+    [Table("Vehiculo")]
+    internal class VehiculoModel
+    {
+        [AutoIncrement, PrimaryKey]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Marca { get; set; }
+        [MaxLength(100)]
+        public string Modelo { get; set; }
+        public int AnioFabricacion { get; set; }
+        [MaxLength(10)]
+        public string Placa { get; set; }
+    }
+}
