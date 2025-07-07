3882dd62c2ad5a07611ea6422a8c5f40371398f9
Nicolasre18 <nicolasre18@gmail.com>
2025-07-07 07:13:08 -0500

    Add project files.

---
+++ RecaldeNExamen3/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RecaldeNExamen3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ RecaldeNExamen3/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RecaldeNExamen3
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
